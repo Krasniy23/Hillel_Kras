a353c936cf752935cf1a24aa4ffb64298e62ecab
krasniy23 <krasnoschek23@gmail.com>
2020-08-31 09:12:00 +0300

    commit4

--- Les1.py
+++ Les1.py
@@ -4,28 +4,20 @@ result = pyfiglet.figlet_format("D I M A", font="3-d")
 print(result)
 
 
-
-myString = "\a"
-print(myString)
-myString = "\b"
-print(myString)
-myString = "\n"
-print(myString)
-myString = "\t"
-print(myString)
-myString = "\"\""
-print(myString)
-myString = "\""
-print(myString)
-myString = "\'"
-print(myString)
+myString1 = "\a"
+myString2 = "\b"
+myString3 = "\n"
+myString4 = "\t"
+myString5 = "\"\""
+myString6 = "\""
+myString7 = "\'"
+print(myString1, myString2, myString3, myString4, myString5, myString6, myString7)
 
 
-x = 10
-y = 2
-a = x + y
-z = x / y
-n = x % y
-m = x ^ y
-print(x, y, a, z, n, m)
-
+num1 = input("x: ")
+num2 = input("y: ")
+num3 = x / y
+num4 = x + y
+num5 = x % y
+num6 = x ^ y
+print(num1, num2, num3, num4, num5, num6)
