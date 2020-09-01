a3cfae7bd939b46c9db5354ca0e037612baefe9c
krasniy23 <krasnoschek23@gmail.com>
2020-09-01 15:53:47 +0300

    les2

--- Les1.py
+++ Les1.py
@@ -3,19 +3,18 @@ import pyfiglet
 result = pyfiglet.figlet_format("D I M A", font="3-d")
 print(result)
 
-
-myString1 = "\a"
-myString2 = "\b"
-myString3 = "\n"
-myString4 = "\t"
-myString5 = "\"\""
-myString6 = "\""
-myString7 = "\'"
+myString1 = "\a     Bell(alert)"
+myString2 = "\b     Backspace"
+myString3 = "\n     New line"
+myString4 = "\t     Horizontal tab"
+myString5 = "\\     Backslash\\n"
+myString6 = '\\"    Double quotation mark"\n'
+myString7 = "\\'     Single quotation mark'"
 print(myString1, myString2, myString3, myString4, myString5, myString6, myString7)
 
 
-num1 = input("x: ")
-num2 = input("y: ")
+num1 = input("Enter x: ")
+num2 = input("Enter y: ")
 num3 = x / y
 num4 = x + y
 num5 = x % y
