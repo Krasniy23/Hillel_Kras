4d8bc05327e03d2c128f81efb3330f8985f85ba9
krasniy23 <krasnoschek23@gmail.com>
2020-09-01 16:54:56 +0300

    Done

---
+++ lesson_2/Les2_2.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    n = int(input('Please enter an integer number: '))
+    print('The next number for the number ' + str(n) + ' is ' + str(n + 1) + '.')
+    print('The previous number for the number ' + str(n) + ' is ' + str(n - 1) + '.')
