8fa1c947e436ee8943113d059dc5f934bc6f5bf7
krasniy23 <krasnoschek23@gmail.com>
2020-09-02 18:40:48 +0300

    Done

--- lesson_2/les2_6.py
+++ lesson_2/les2_6.py
@@ -1,3 +1,6 @@
-x = int(input("Enter number: "))
-x = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-print(x)
+if __name__ == '__main__':
+    x = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    print(input("Enter: "))
+    print(abs(all([x])))
+    if x is not [x]:
+        print()
