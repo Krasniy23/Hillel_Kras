0ecd73e19386a0bd4538d32339407571b331172e
krasniy23 <krasnoschek23@gmail.com>
2020-09-03 18:38:06 +0300

    Done3

--- lesson_2/les2_6.py
+++ lesson_2/les2_6.py
@@ -1,6 +1,6 @@
-if __name__ == '__main__':
-    x = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    print(input("Enter: "))
-    print(abs(all([x])))
-    if x is not [x]:
-        print()
+number = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+x = abs(int(float(input())))
+if x in number:
+    print(x)
+else:
+    print('not enter')
