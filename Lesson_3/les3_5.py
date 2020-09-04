538fcfc106fe3e52cf71b6fa7511f628f7289552
Krasniy23 <krasnoschek23@gmail.com>
2020-09-04 21:47:26 +0300

    Done

--- Lesson_3/les3_5.py
+++ Lesson_3/les3_5.py
@@ -2,10 +2,10 @@ import random
 num = int(input("Enter number: "))
 numbers = random.sample(range(10), 1)
 for data in numbers:
-    if num <= data:
-        print("You WON!")
-        break
-    if num >= data:
+    if num != data:
         print("You lose!")
+        break
+    if num == data:
+        print("You WON!")
         break
 print(''.join(map(str, numbers)))
