8ccad8e405a0cf3a2a9514cb839caa04f5a9c428
Krasniy23 <krasnoschek23@gmail.com>
2020-09-04 22:18:35 +0300

    Доделать

--- Lesson_3/les3_4.py
+++ Lesson_3/les3_4.py
@@ -1,6 +1,5 @@
 num_list = [100, 106, 112, 118, 124, 130, 136, 142, 148, 154, 160, 166, 172, 178, 184, 190, 196]
 for num in num_list:
-    num_list = num / 5
-    if num > 150:
-        print(":(")
-    print(num_list)
+    if num % 5 == 0 or num <= 150:
+        print()
+print(num_list)
