329a9565b9a20c0044475ad981a33de39219b6c7
Krasniy23 <krasnoschek23@gmail.com>
2020-09-07 15:46:32 +0300

    Done

--- Lesson_3/Les3_6.py
+++ Lesson_3/Les3_6.py
@@ -2,7 +2,8 @@ x1 = int(input())
 y1 = int(input())
 x2 = int(input())
 y2 = int(input())
-if x1 - 1 == x2 or x1 + 1 == x2 and y1 - 2 == y2 or y1 + 2 == y2 and x1 - 2 == x2 or x1 + 2 == x2 and y1 - 1 == y2 or y1 + 1 == y2:
+if x1 - 1 == x2 or x1 + 1 == x2 and y1 - 2 == y2 or y1 + 2 == y2 \
+        and x1 - 2 == x2 or x1 + 2 == x2 and y1 - 1 == y2 or y1 + 1 == y2:
     print("Can go")
 else:
     print("Can't go")
