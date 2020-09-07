e8a77f696ecfd6459295ac1f122312fffa3e4682
Krasniy23 <krasnoschek23@gmail.com>
2020-09-07 10:00:02 +0300

    Done

--- Lesson_3/les3_5.py
+++ Lesson_3/les3_5.py
@@ -3,7 +3,7 @@ num = int(input("Enter number: "))
 numbers = random.sample(range(10), 1)
 for data in numbers:
     if num != data:
-        print("You lose!")
+        print("You Lose!")
         break
     if num == data:
         print("You WON!")
