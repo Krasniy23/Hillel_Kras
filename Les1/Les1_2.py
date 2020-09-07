6530c479539373b39ffc69870d1ebe0b79de5efe
Krasniy23 <krasnoschek23@gmail.com>
2020-09-07 12:44:15 +0300

    Done

---
+++ Les1/Les1_2.py
@@ -0,0 +1,8 @@
+myString1 = "\a     Bell(alert)"
+myString2 = "\b     Backspace"
+myString3 = "\n     New line"
+myString4 = "\t     Horizontal tab"
+myString5 = "\\     Backslash\\n"
+myString6 = '\\"    Double quotation mark"\n'
+myString7 = "\\'     Single quotation mark'"
+print(myString1, myString2, myString3, myString4, myString5, myString6, myString7)
---
+++ Les1/Les1_3.py
@@ -0,0 +1,7 @@
+num1 = input("Enter x: ")
+num2 = input("Enter y: ")
+num3 = x / y
+num4 = x + y
+num5 = x % y
+num6 = x ^ y
+print(num1, num2, num3, num4, num5, num6)
