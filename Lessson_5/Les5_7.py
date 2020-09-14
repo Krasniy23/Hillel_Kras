65f52294b1902ed07a438c600677cde84c0119d4
Krasniy23 <krasnoschek23@gmail.com>
2020-09-14 15:10:41 +0300

    Done

---
+++ Lessson_5/Les5_7.py
@@ -0,0 +1,11 @@
+import datetime
+
+date = int(input("Введите день: "))
+month = int(input("Введите месяц: "))
+year = int(input("Введите год: "))
+try:
+    data = datetime.date(year, month, date)
+    print(data)
+    print("True")
+except:
+    print("False")
