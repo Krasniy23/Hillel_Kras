ad56c932e8a7270b3de143c5b7ebaacbb4ce3deb
Krasniy23 <krasnoschek23@gmail.com>
2020-09-16 09:17:16 +0300

    halfdone

---
+++ Lesson_6/Les6_1.py
@@ -0,0 +1,3 @@
+coin = ('Bitcoin', 'Ether', 'Ripple', 'Litecoin')
+code = ('BTC', 'ETH', 'XRP', 'LTC')
+print([i + l for i, l in zip(coin, code)])
---
+++ Lesson_6/Les6_2.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+now = datetime.now()
+time = now.strftime("%H:%M:%S")
+print()
+print("time is now:", time)
