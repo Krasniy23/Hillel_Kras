38f21d27e06481ed205aa7683d42955a04bb32c6
Krasniy23 <54262807+Krasniy23@users.noreply.github.com>
2020-09-17 19:43:42 +0300

    halfdone

--- Lesson_6/Les6_4.py
+++ Lesson_6/Les6_4.py
@@ -1,6 +1,8 @@
 import json
+from datetime import timedelta
 from pprint import pprint
 
-with open("acdc.json", "r") as f:
-    data = json.loads(f.read())
-    pprint(data["name"])
+with open("acdc.json") as f:
+    data = json.load(f)
+    duration = [int(track['duration']) for track in data['album']['track']['track']]
+    pprint(timedelta(seconds=sum(duration)
