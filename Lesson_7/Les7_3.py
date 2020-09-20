877a194647a031f4ab52891a259d4774985a68bd
Krasniy23 <54262807+Krasniy23@users.noreply.github.com>
2020-09-20 16:53:38 +0300

    halfdone

---
+++ Lesson_7/Les7_3.py
@@ -0,0 +1,17 @@
+from time import strftime
+import requests
+from pprint import pprint
+from datetime import datetime
+
+now = datetime.now()
+city_name = input('Enter the city name: ')
+API_ID = 'f9ada9efec6a3934dad5f30068fdcbb8'
+
+weather_request = requests.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=city&cnt=5&units=metric&appid',
+                               params={'q': city_name, 'units': 'metric', 'cnt': 5, 'lang': 'ru', 'appid': API_ID})
+weather_data = weather_request.json()
+pprint(weather_data)
+
+with open('20-09-2020-Odessa-5-days-weather-forecast.txt', 'w', encoding="utf-8") as file_object:
+    weather_data = fileobject.write(strftime("%d-%m-%Y"))
+pprint(weather_data)
