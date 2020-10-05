924981f89a557c77e0148479f472f16606072308
Krasniy23 <54262807+Krasniy23@users.noreply.github.com>
2020-10-05 10:39:43 +0300

    Done

--- Lesson_5/Les5_1.py
+++ Lesson_5/Les5_1.py
@@ -1,11 +1,11 @@
-from cmath import sqrt
+import math
 
 def is_prime(number):
     if number % 2 == 0 and number != 2:
         return False
     if number == 0 or number == 1:
         return False
-    for n in range(3, int(sqrt(number).real) + 1, 2):
+    for n in range(3, int(math.sqrt(number) + 1), 2):
         if number % n == 0:
             return False
     return True
