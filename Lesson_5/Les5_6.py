797c08efc8583fb7fb8a851a783f50f7c0c3ba4c
Krasniy23 <54262807+Krasniy23@users.noreply.github.com>
2020-10-05 10:54:58 +0300

    Done

--- Lesson_5/Les5_6.py
+++ Lesson_5/Les5_6.py
@@ -1,5 +1,3 @@
 dict1 = {'first_color': 'Red', 'second_color': 'Green', 'third_color': None}
-for k, v in dict1.items():
-    if not any([None]):
-        del dict1[k]
-print(k)
+dict2 = {key: value for key, value in dict1.items() if value is not None}
+print(dict2)
